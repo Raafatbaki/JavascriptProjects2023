770aa1051cfb95e4f5c21b33f8ef2cab721ba855
Raafatbaki <raafat.baki.93@gmail.com>
2023-06-19 22:38:44 +0200

    commit

---
+++ script.js
@@ -0,0 +1,41 @@
+axios.get('https://tarmeezacademy.com/api/v1/posts')
+  .then(function (response) {
+    // handle success
+    const posts = response.data.data
+    for(post of posts){
+      console.log(post.body);
+      let content = `
+      <div class="card shadow">
+        <div class="card-header">
+          <img class="rounded-circle border border-2" src="profile-pic/pngwing.com.png" alt="" style="width: 40px; height: 40px;">
+          <b>@Raafat</b>
+        </div>
+        <div class="card-body">
+          <img class="w-100" src="placeholders/image.jpg" alt="">
+          <h6 style="color: rgb(193, 193, 193);" class="mt-1">
+            2 min ago
+          </h6>
+          <h5>
+            Hallo Word
+          </h5>
+          <p>${post.body}</p>
+          <hr>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+              <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+            </svg>
+            <span>
+              (3) Comments
+            </span>
+          </div>
+        </div>
+      </div>`
+
+    document.getElementById("posts").innerHTML += content
+    }
+    
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
