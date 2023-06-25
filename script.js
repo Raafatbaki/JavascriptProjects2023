3c5bb2074160c752553e6d5df3ee8a62013e40a0
Raafatbaki <raafat.baki.93@gmail.com>
2023-06-25 19:25:54 +0200

    commit

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-
-
 const baseurl = "https://tarmeezacademy.com/api/v1"
-const urlParams = new URLSearchParams(window.location.search)
-const id = urlParams.get("postId")
-console.log(id)
+
 setupUI()
 getPost()
 
@@ -253,13 +249,13 @@ function getCurrentUser()
   return user
 }
 
-
 function postClicked(postId)
 {
   window.location =`postDetails.html?postId=${postId}`
 }
-
-
+const urlParams = new URLSearchParams(window.location.search)
+const id = urlParams.get("postId")
+console.log(id)
 
 getDetailsPost()
 function getDetailsPost()
@@ -302,6 +298,10 @@ function getDetailsPost()
     //     </div>
     //   </div>
     // `
+    let postTitel = ""
+    if(post.titel != null){
+      postTitel = post.titel
+    }
     const postContent =
     `
       <div class="col-9"  id="post-details">
@@ -325,16 +325,16 @@ function getDetailsPost()
               ${post.created_at}
               </h6>
               <h5>
-              ${post.body}
+              ${postTitel}
               </h5>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae fugit consequuntur aut odit ducimus repudiandae accusamus illo aliquid! Ea necessitatibus, enim consectetur voluptate ut expedita dolorum tenetur. Consequatur, voluptas dicta.</p>
+              <p>${post.body}</p>
               <hr>
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
                   <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
                 </svg>
                 <span>
-                  (3) Comments
+                  (${post.comments_count}) Comments
                   <span>
                     <button class="btn btn-sm rounded" style="background-color: gray; color: white;"></button>
                   </span>
