60842add040bb6d5950f7f8b1e4ded28edd4a988
Raafatbaki <raafat.baki.93@gmail.com>
2023-06-25 20:16:05 +0200

    commit

--- script.js
+++ script.js
@@ -254,8 +254,9 @@ function postClicked(postId)
   window.location =`postDetails.html?postId=${postId}`
 }
 const urlParams = new URLSearchParams(window.location.search)
+//console.log(urlParams)
 const id = urlParams.get("postId")
-console.log(id)
+//console.log(id)
 
 getDetailsPost()
 function getDetailsPost()
@@ -266,45 +267,16 @@ function getDetailsPost()
     const post = response.data.data
     const comments = post.comments
     const author = post.author
-    // document.getElementById("username-span").innerHTML = author.username
-    // const postContent = 
-    // `
-    //   <div class="card shadow">
-    //     <div class="card-header">
-    //       <img class="rounded-circle border border-2" src="${author.profile_image}" alt="" style="width: 40px; height: 40px;">
-    //       <b>@${author.username}</b>
-    //     </div>
-    //     <div class="card-body">
-    //       <img class="w-100" src="${post.image}" alt="">
-    //       <h6 style="color: rgb(193, 193, 193);" class="mt-1">
-    //         ${post.created_at}
-    //       </h6>
-    //       <h5>
-    //         ${post.body}
-    //       </h5>
-    //       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae fugit consequuntur aut odit ducimus repudiandae accusamus illo aliquid! Ea necessitatibus, enim consectetur voluptate ut expedita dolorum tenetur. Consequatur, voluptas dicta.</p>
-    //       <hr>
-    //       <div>
-    //         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-    //           <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-    //         </svg>
-    //         <span>
-    //           (3) Comments
-    //           <span>
-    //             <button class="btn btn-sm rounded" style="background-color: gray; color: white;"></button>
-    //           </span>
-    //         </span>
-    //       </div>
-    //     </div>
-    //   </div>
-    // `
+
     let postTitel = ""
     if(post.titel != null){
       postTitel = post.titel
     }
     const postContent =
     `
-      <div class="col-9"  id="post-details">
+    <div class="container" style="height: 1000px;">
+      <div class="d-flex justify-content-center mt-5">
+        <div class="col-9">
           <!-- USER'S POST -->
           <h1>
             <span id="username-span">
@@ -345,7 +317,9 @@ function getDetailsPost()
           <!-- // POST ROW // -->
           <!-- // USER'S POST // -->
         </div>
-      `
+      </div>
+    </div>
+    `
     document.getElementById("post-details").innerHTML = postContent
 
 
@@ -358,4 +332,3 @@ function getDetailsPost()
 }
 
 
-    
