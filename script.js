3a98f6233f18e884a89e49f8de65ae08a478e5bd
Raafatbaki <raafat.baki.93@gmail.com>
2023-06-28 21:17:00 +0200

    commit

--- script.js
+++ script.js
@@ -104,10 +104,11 @@ function getPost(relode = true, page = 1)
       
     }
   })
-  .catch(function (error) {
-    const message = error.response.data.message
-    showAlert(message, "danger")
-  }).finally(() => {
+  // .catch(function (error) {
+  //   const message = error.response.data.message
+  //   showAlert(message, "danger")
+  // })
+  .finally(() => {
     toggleLoader(false)
   })
 }
@@ -298,112 +299,111 @@ function postClicked(postId)
 const urlParams = new URLSearchParams(window.location.search)
 const id = urlParams.get("postId")
 
-getDetailsPost()
-function getDetailsPost()
-{
-  toggleLoader(true)
-  axios.get(`${baseurl}/posts/${id}`)
-  .then((response) => {
-    //console.log(response.data)
-    const post = response.data.data
-    const comments = post.comments
-    const author = post.author
+// getDetailsPost()
+// function getDetailsPost()
+// {
+//   toggleLoader(true)
+//   axios.get(`${baseurl}/posts/${id}`)
+//   .then((response) => {
+//     //console.log(response.data)
+//     const post = response.data.data
+//     const comments = post.comments
+//     const author = post.author
 
-    let postTitel = ""
-    if(post.titel != null){
-      postTitel = post.titel
-    }
-    let commentsContent = ""
-    for(comment of comments)
-    {
-      commentsContent +=
-        `
-        <!-- COMMENT -->
-        <div class="p-3" style="background-color: rgb(235, 235, 235);">
-          <!-- PROFILE PIC + USERNAME -->
-          <div>
-            <img src="${comment.author.profile_image}" class="rounded-circle" alt="" style="width: 40px; height: 40px;">
-            <b>${comment.author.username}</b>
-          </div>
-          <!-- // PROFILE PIC + USERNAME // -->
-          <!-- COMMENT'S BODY -->
-          <div>
-            ${comment.body}
-          </div>
-          <!-- // COMMENT'S BODY // -->
-        </div>
-        <!-- // COMMENT // -->
-        `
-    }
+//     let postTitel = ""
+//     if(post.titel != null){
+//       postTitel = post.titel
+//     }
+//     let commentsContent = ""
+//     for(comment of comments)
+//     {
+//       commentsContent +=
+//         `
+//         <!-- COMMENT -->
+//         <div class="p-3" style="background-color: rgb(235, 235, 235);">
+//           <!-- PROFILE PIC + USERNAME -->
+//           <div>
+//             <img src="${comment.author.profile_image}" class="rounded-circle" alt="" style="width: 40px; height: 40px;">
+//             <b>${comment.author.username}</b>
+//           </div>
+//           <!-- // PROFILE PIC + USERNAME // -->
+//           <!-- COMMENT'S BODY -->
+//           <div>
+//             ${comment.body}
+//           </div>
+//           <!-- // COMMENT'S BODY // -->
+//         </div>
+//         <!-- // COMMENT // -->
+//         `
+//     }
 
-    const postContent =
-    `
-    <div class="container" style="height: 1000px;">
-      <div class="d-flex justify-content-center mt-5">
-        <div class="col-9">
-          <!-- USER'S POST -->
-          <h1>
-            <span id="username-span">
-              ${author.username}
-            </span>
-            post
-          </h1>
-          <!-- // USER'S POST // -->
-          <!-- POST ROW -->
-          <div class="card shadow">
-            <div class="card-header">
-              <img class="rounded-circle border border-2" src="${author.profile_image}" alt="" style="width: 40px; height: 40px;">
-              <b>@${author.username}</b>
-            </div>
-            <div class="card-body">
-              <img class="w-100" src="${post.image}" alt="">
-              <h6 style="color: rgb(193, 193, 193);" class="mt-1">
-              ${post.created_at}
-              </h6>
-              <h5>
-              ${postTitel}
-              </h5>
-              <p>${post.body}</p>
-              <hr>
-              <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-                </svg>
-                <span>
-                  (${post.comments_count}) Comments
-                  <span>
-                    <button class="btn btn-sm rounded" style="background-color: gray; color: white;"></button>
-                  </span>
-                </span>
-              </div>
-            </div>
-            <div id="comments">
-              ${commentsContent}
-            </div>
-            <!-- ADD COMMENT -->
-            <div class="input-group mb-3" id="add-comment-div">
-              <input type="text" id="comment-input" placeholder="add your comment here.." class="form-control">
-              <button class="btn btn-outline-primary" type="button" onclick="createCommentClicked()">send</button>
-            </div>
-            <!-- // ADD COMMENT // -->
-          </div>
-          <!-- // POST ROW // -->
-          <!-- // USER'S POST // -->
-        </div>
-      </div>
-    </div>
-    `
-    document.getElementById("post-details").innerHTML = postContent
-
-
-  })
-  .catch(function (error) {
-    const message = error.response.data.message
-    showAlert(message, "danger")
-  }).finally(() => {
-    toggleLoader(false)
-  })
-}
+//     const postContent =
+//     `
+//     <div class="container" style="height: 1000px;">
+//       <div class="d-flex justify-content-center mt-5">
+//         <div class="col-9">
+//           <!-- USER'S POST -->
+//           <h1>
+//             <span id="username-span">
+//               ${author.username}
+//             </span>
+//             post
+//           </h1>
+//           <!-- // USER'S POST // -->
+//           <!-- POST ROW -->
+//           <div class="card shadow">
+//             <div class="card-header">
+//               <img class="rounded-circle border border-2" src="${author.profile_image}" alt="" style="width: 40px; height: 40px;">
+//               <b>@${author.username}</b>
+//             </div>
+//             <div class="card-body">
+//               <img class="w-100" src="${post.image}" alt="">
+//               <h6 style="color: rgb(193, 193, 193);" class="mt-1">
+//               ${post.created_at}
+//               </h6>
+//               <h5>
+//               ${postTitel}
+//               </h5>
+//               <p>${post.body}</p>
+//               <hr>
+//               <div>
+//                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+//                   <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+//                 </svg>
+//                 <span>
+//                   (${post.comments_count}) Comments
+//                   <span>
+//                     <button class="btn btn-sm rounded" style="background-color: gray; color: white;"></button>
+//                   </span>
+//                 </span>
+//               </div>
+//             </div>
+//             <div id="comments">
+//               ${commentsContent}
+//             </div>
+//             <!-- ADD COMMENT -->
+//             <div class="input-group mb-3" id="add-comment-div">
+//               <input type="text" id="comment-input" placeholder="add your comment here.." class="form-control">
+//               <button class="btn btn-outline-primary" type="button" onclick="createCommentClicked()">send</button>
+//             </div>
+//             <!-- // ADD COMMENT // -->
+//           </div>
+//           <!-- // POST ROW // -->
+//           <!-- // USER'S POST // -->
+//         </div>
+//       </div>
+//     </div>
+//     `
+//     document.getElementById("post-details").innerHTML = postContent
+//   })
+//   .catch(function (error) {
+//     const message = error.response.data.message
+//     showAlert(message, "danger")
+//   })
+//   .finally(() => {
+//     toggleLoader(false)
+//   })
+// }
 
 function createCommentClicked() 
 {
@@ -498,118 +498,119 @@ function confirmPostDelete()
   })
 }
 
-getUser()
-function getUser()
-{
-  const id = getCurrentUserId()
-  toggleLoader(true)
-  axios.get(`${baseurl}/users/${id}`)
-  .then((response) => {
-    const user = response.data.data
-    document.getElementById("main-info-email").innerHTML = user.email
-    document.getElementById("main-info-name").innerHTML = user.name
-    document.getElementById("main-info-username").innerHTML = user.username
-    document.getElementById("posts-count").innerHTML = user.posts_count
-    document.getElementById("comments-count").innerHTML = user.comments_count
-    document.getElementById("header-image").src = user.profile_image
-    document.getElementById("name-posts").innerHTML = `${user.username}'s`
-  })
-  .catch(function (error) {
-    const message = error.response.data.message
-    showAlert(message, "danger")
-  }).finally(() => {
-    toggleLoader(false)
-  })
-}
+// getUser()
+// function getUser()
+// {
+//   const id = getCurrentUserId()
+//   toggleLoader(true)
+//   axios.get(`${baseurl}/users/${id}`)
+//   .then((response) => {
+//     const user = response.data.data
+//     document.getElementById("main-info-email").innerHTML = user.email
+//     document.getElementById("main-info-name").innerHTML = user.name
+//     document.getElementById("main-info-username").innerHTML = user.username
+//     document.getElementById("posts-count").innerHTML = user.posts_count
+//     document.getElementById("comments-count").innerHTML = user.comments_count
+//     document.getElementById("header-image").src = user.profile_image
+//     document.getElementById("name-posts").innerHTML = `${user.username}'s`
+//   })
+//   .catch(function (error) {
+//     const message = error.response.data.message
+//     showAlert(message, "danger")
+//   })
+//   .finally(() => {
+//     toggleLoader(false)
+//   })
+// }
 
-getPosts()
-function getPosts()
-{
-  const id = getCurrentUserId()
-  toggleLoader(true)
-  axios.get(`${baseurl}/users/${id}/posts`)
-  .then(function (response) {
-    // handle success
-    const posts = response.data.data
-    document.getElementById("user-posts").innerHTML = ""
+// getPosts()
+// function getPosts()
+// {
+//   const id = getCurrentUserId()
+//   toggleLoader(true)
+//   axios.get(`${baseurl}/users/${id}/posts`)
+//   .then(function (response) {
+//     // handle success
+//     const posts = response.data.data
+//     document.getElementById("user-posts").innerHTML = ""
 
-    for(post of posts){
-      const author = post.author
-      let postTitel = ""
-      if(post.titel != null){
-        postTitel = post.titel
-      }
+//     for(post of posts){
+//       const author = post.author
+//       let postTitel = ""
+//       if(post.titel != null){
+//         postTitel = post.titel
+//       }
+//       // SHOW OR HIDE EDIT BTNTON
+//       let user = getCurrentUser()
+//       let isMyPost = user != null && post.author.id == user.id
+//       let editBtnContent = ``
+//       if(isMyPost)
+//       {
+//         editBtnContent = 
+//         `
+//           <button class='btn btn-danger' style='margin-left: 5px; float: right' onclick="deletePostBtnClicked('${encodeURIComponent(JSON.stringify(post))}')">delete</button>
+//           ​<button class='btn btn-secondary' style='float: right' onclick="editPostBtnClicked('${encodeURIComponent(JSON.stringify(post))}')">edit</button>
+//         `
+//       }
 
-      // SHOW OR HIDE EDIT BTNTON
-      let user = getCurrentUser()
-      let isMyPost = user != null && post.author.id == user.id
-      let editBtnContent = ``
-      if(isMyPost)
-      {
-        editBtnContent = 
-        `
-          <button class='btn btn-danger' style='margin-left: 5px; float: right' onclick="deletePostBtnClicked('${encodeURIComponent(JSON.stringify(post))}')">delete</button>
-          ​<button class='btn btn-secondary' style='float: right' onclick="editPostBtnClicked('${encodeURIComponent(JSON.stringify(post))}')">edit</button>
-        `
-      }
+//       let content = `
+//       <div class="card shadow">
+//         <div class="card-header">
+//           <img class="rounded-circle border border-2" src="${author.profile_image}" alt="" style="width: 40px; height: 40px;">
+//           <b>${author.username}</b>
+//           ${editBtnContent}
+//         </div>
+//         <div class="card-body" onclick="postClicked(${post.id})" style="cursor : pointer">
+//           <img class="w-100" src="${post.image}" alt="">
+//           <h6 style="color: rgb(193, 193, 193);" class="mt-1">
+//             ${post.created_at}
+//           </h6>
+//           <h5>
+//             ${postTitel}
+//           </h5>
+//           <p>${post.body}</p>
+//           <hr>
+//           <div>
+//             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+//               <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+//             </svg>
+//             <span>
+//               (${post.comments_count}) Comment
+//               <span id="post-tags-${post.id}">
 
-      let content = `
-      <div class="card shadow">
-        <div class="card-header">
-          <img class="rounded-circle border border-2" src="${author.profile_image}" alt="" style="width: 40px; height: 40px;">
-          <b>${author.username}</b>
-          ${editBtnContent}
-        </div>
-        <div class="card-body" onclick="postClicked(${post.id})" style="cursor : pointer">
-          <img class="w-100" src="${post.image}" alt="">
-          <h6 style="color: rgb(193, 193, 193);" class="mt-1">
-            ${post.created_at}
-          </h6>
-          <h5>
-            ${postTitel}
-          </h5>
-          <p>${post.body}</p>
-          <hr>
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-              <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-            </svg>
-            <span>
-              (${post.comments_count}) Comment
-              <span id="post-tags-${post.id}">
+//               </span>
+//             </span>
+//           </div>
+//         </div>
+//       </div>`
 
-              </span>
-            </span>
-          </div>
-        </div>
-      </div>`
+//       document.getElementById("user-posts").innerHTML += content
 
-      document.getElementById("user-posts").innerHTML += content
+//       const currentPostTagsId = `post-tags-${post.id}`
+//       document.getElementById(currentPostTagsId).innerHTML = ""
 
-      const currentPostTagsId = `post-tags-${post.id}`
-      document.getElementById(currentPostTagsId).innerHTML = ""
-
-      for(tag of post.tags)
-      {
-        //console.log(tag.name)
-        let tagsContent = 
-        ` <button class="btn btn-sm rounded" style="background-color: gray; color: white;">
-            ${tag.name}
-          </button>
-        `
-        document.getElementById(currentPostTagsId).innerHTML += tagsContent
-      }
-      //console.log(post.tags)
+//       for(tag of post.tags)
+//       {
+//         //console.log(tag.name)
+//         let tagsContent = 
+//         ` <button class="btn btn-sm rounded" style="background-color: gray; color: white;">
+//             ${tag.name}
+//           </button>
+//         `
+//         document.getElementById(currentPostTagsId).innerHTML += tagsContent
+//       }
+//       //console.log(post.tags)
       
-    }
-  })
-  .catch(function (error) {
-    const message = error.response.data.message
-    showAlert(message, "danger")
-  }).finally(() => {
-    toggleLoader(false)
-  })
-}
+//     }
+//   })
+//   .catch(function (error) {
+//     const message = error.response.data.message
+//     showAlert(message, "danger")
+//   })
+//   .finally(() => {
+//     toggleLoader(false)
+//   })
+// }
 
 function userClicked(userId)
 {
